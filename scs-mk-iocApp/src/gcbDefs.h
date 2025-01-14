9423fc6b6a9236cf147e4ad2e199b78c0ec07fdf
Ignacio Arriagada <iarriagada@gemini.edu>
2025-01-14 12:00:14 -0500

    Added infrastructure for command record and status client

---
+++ scs-mk-iocApp/src/gcbDefs.h
@@ -0,0 +1,50 @@
+#ifndef GCBDEFS_H
+#define GCBDEFS_H
+
+#ifdef __cplusplus
+#include <gcbCommand/gcbCommandRecord.h>
+#include <gcbCommand/gcbStatusRecord.h>
+#include <epicsEvent.h>
+#endif
+#include <gcbCommand/gcbSynchro.h>
+
+#ifdef __cplusplus
+/* The Cpp global variables  */
+using namespace epics::gcb::gcbCommand;
+using namespace epics::gcb::gcbStatus;
+
+//extern EpicsEventPtr rxTrigger;
+extern EpicsEventPtr rxP1Trigger;
+//extern EpicsMutexPtr rwMemSemaphore;
+extern EpicsMutexPtr rwPage1Sem;
+extern EpicsEventPtr extReadThreadExit;
+extern EpicsMutexPtr threadCountSem;
+#endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    /* Functions and shared memory to be used by Cpp and C stuff  */
+    //extern gcbBlock *page0;
+    extern int threadCounter;
+    extern statusBlock *page1;
+    //extern int cmdBlockEventWait();
+    //extern void cmdBlockMutexLock();
+    //extern void cmdBlockMutexUnlock();
+    //extern void gcbStatusProcess(statusBlock *refmem);
+    //extern void statusBlockTrigger();
+    extern int externalReadThreadExitEventWait();
+    extern int stsBlockEventWait();
+    extern void stsBlockMutexLock();
+    extern void stsBlockMutexUnlock();
+    extern void threadCntAdd();
+    extern void threadCntSubstract();
+    extern void triggerGCBfromC();
+    extern void gcbProcess(sharedMem *refmem);
+    extern void exitGracefully();
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GCBDEFS_H */
---
+++ scs-mk-iocApp/src/gcbInterface.cpp
@@ -0,0 +1,366 @@
+/*
+ * @author iarriagada
+ * @date 20231016
+ */
+
+#include <cstddef>
+#include <cstdlib>
+#include <cstddef>
+#include <memory>
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <iocsh.h>
+#include <epicsThread.h>
+#include <epicsEvent.h>
+#include <epicsExit.h>
+#include <pv/lock.h>
+#include <pv/pvDatabase.h>
+#include <gcbCommand/gcbThread.h>
+#include <gcbCommand/gcbCommandRecord.h>
+#include <gcbCommand/gcbStatusRecord.h>
+#include <gcbCommand/gcbSynchro.h>
+#include <shareLib.h>
+
+#include <epicsExport.h>
+#include "gcbDefs.h"
+#define epicsExportSharedSymbols
+
+using namespace epics::pvDatabase;
+using namespace epics::pvData;
+using namespace epics::gcb::gcbCommand;
+using namespace epics::gcb::gcbStatus;
+using namespace epics::gcb::gcbThread;
+using std::string;
+
+/* Global pointer to gcb record */
+GCBCommandRecordPtr gcb_rec = NULL;
+/* Global pointer to an EPICS thread */
+GCBThreadPtr superThread;
+/* Global pointers to GCB internal thread flags */
+EpicsEventPtr rtTrigger = EpicsEventPtr(new epics::pvData::Event());
+EpicsEventPtr rtStop = EpicsEventPtr(new epics::pvData::Event());
+
+/* Global pointer to Status Monitor */
+GCBStatusClientPtr gcb_sts_client = NULL;
+statusBlock *page1 = new statusBlock();
+EpicsEventPtr rxP1Trigger = EpicsEventPtr(new epics::pvData::Event());
+EpicsEventPtr extReadThreadExit = EpicsEventPtr(new epics::pvData::Event());
+EpicsMutexPtr rwPage1Sem = EpicsMutexPtr(new epicsMutex());
+EpicsMutexPtr threadCountSem = EpicsMutexPtr(new epicsMutex());
+int threadCounter = 0;
+/* Initialize a pvClient using the PVAccess */
+PvaClientPtr gcbStsPva = PvaClient::get("pva");
+
+/*
+ * @brief Initialize a custom PV GCBCommand Record. A global pointer to 
+ * the record will be created.
+ * 
+ * @param recName: Name of the GCBCommand record.
+ * This name can be used to identify the record in the database.
+ * */
+void initGcbCmdRecord(string recName)
+{
+    gcb_rec = GCBCommandRecord::create(recName);
+    bool result = PVDatabase::getMaster()->addRecord(gcb_rec);
+    if(!result) std::cout << string(recName) << " not added" << "\n";
+}
+
+/*
+ * @brief Initialize client GCB Record. This creates a client type record,
+ * with the purpose of monitoring a server GCB Status record. It uses the
+ * the global pointer to a pvaClient object that makes the pva connection.
+ * It also initializes the semaphore and the monitor flag to be used by the shared
+ *
+ * @param pvrName the name of the record to be monitored
+ * */
+/* In the diagram: gcbStsClient() */
+void initGCBStatusClientRec(std::string pvrName)
+{
+    if (gcb_sts_client)
+    {
+        std::cout << "GCB Monitor has already been initialized...\n";
+        return;
+    }
+    gcb_sts_client  = GCBStatusClient::createClient(pvrName, gcbStsPva);
+}
+
+/*
+ * @brief Initializes the semaphore, monitor flag and shared page0.
+ * After it passes the pointers to the gcbClient to be assigned to
+ * the proper attributes.
+ * */
+void initGCBStatusDataShare()
+{
+    if(!gcb_sts_client)
+    {
+        std::cout << "The GCB Status client has not been initialized";
+        return;
+    }
+    
+    gcb_sts_client->initDataSharing(page1, rwPage1Sem, rxP1Trigger);
+}
+
+/*
+ * @brief Start the internal thread from the gcbStatusClient
+ * */
+void startGCBStatusClientThread()
+{
+    if(!gcb_sts_client)
+    {
+        std::cout << "The GCB status client has not been initialized";
+        return;
+    }
+    gcb_sts_client->startThread();
+}
+
+/*
+ * @brief Test function to pass to a thread object
+ * */
+void testRunFunc()
+{
+        std::cout << "Thread Starting!!!\n";
+        while(1)
+        {
+            std::cout << "Thread going to sleep!!!\n";
+            rtTrigger->wait();
+            epicsThreadSleep(.1);
+            std::cout << "The thread has been awoken\n";
+        } 
+}
+
+/*
+ * @brief Initialize an epics thread with name "name".
+ * A pointer will be assigned to the thread, to access the semaphores
+ *
+ * @param name: The name of the thread*/
+void startTest(const char *name)
+{
+    superThread = GCBThreadPtr(new GCBThread(name, testRunFunc));
+    epicsThreadSleep(0.1);
+    superThread->init();
+    superThread->start();
+}
+
+/*
+ * @brief Signal the epics thread to wake up.
+ * The function uses the method signalThread to access the semaphore.
+ * */
+void wakeThread()
+{
+    superThread->signalThread();
+}
+
+/**
+ *  @brief Function that will be accessed by
+ *  the IOC's C infrastructure
+ **/
+void triggerGCBfromC()
+{
+    if(!gcb_rec) {
+        throw std::runtime_error("gcbCommandRecord has not been created");
+        return;
+    }
+    std::cout << "Triggering the GCB record thread\n";
+    gcb_rec->trigger();
+}
+
+/*
+ * @brief Execute the gcbCommandRecord method to write
+ * the contents of the refMemory structure to the record fields
+ **/
+void gcbProcess(sharedMem *refmem)
+{
+    if(!gcb_rec) {
+        throw std::runtime_error("gcbCommandRecord has not been created");
+        return;
+    }
+    //std::cout << "Writting to GCB record thread\n";
+    gcb_rec->processSynch(refmem);
+}
+
+/*
+ * @brief Start the internal GCB Record thread.
+ * Uses the GCB Record method startThread() to initialize the internal record.*/
+void startRecThread()
+{
+    if(!gcb_rec) {
+        throw std::runtime_error("gcbCommandRecord has not been created");
+        return;
+    }
+    gcb_rec->startThread(rtTrigger, rtStop);
+}
+
+/*
+ * @brief Mask to be passed to the C infrastucture to wait for
+ * a signal to the Write Status method
+ * */
+int stsBlockEventWait (){
+    return rxP1Trigger->wait();
+}
+
+/*
+ * @brief Mask to be passed to the C infrastucture to lock the R/W
+ * semaphore
+ * */
+void stsBlockMutexLock(){
+    rwPage1Sem->lock();
+}
+
+/*
+ * @brief Mask to be passed to the C infrastucture to unlock the R/W
+ * semaphore
+ * */
+void stsBlockMutexUnlock(){
+    rwPage1Sem->unlock();
+}
+
+/*
+ * @brief Mask to be passed to the C infrastucture for an external
+ *  read from page 0 thread to get an exit signal
+ * */
+int externalReadThreadExitEventWait (){
+    return extReadThreadExit->tryWait();
+}
+
+/*
+ * @brief Mask to be passed to the C infrastucture to lock the thread counter
+ * variable
+ * */
+void threadCntAdd(){
+    threadCountSem->lock();
+    threadCounter++;
+    threadCountSem->unlock();
+}
+
+/*
+ * @brief Mask to be passed to the C infrastucture to unlock the thread counter
+ * variable
+ * */
+void threadCntSubstract(){
+    threadCountSem->lock();
+    threadCounter--;
+    threadCountSem->unlock();
+}
+
+void exitGracefully(){
+    extReadThreadExit->signal();
+    rxP1Trigger->signal();
+    while(threadCounter){
+        epicsThreadSleep(0.005);
+    }
+    //epicsExit(1);
+}
+
+/* Define all the EPICS console functions */
+
+static const iocshArg recinitArg0 = { "recordName", iocshArgString };
+static const iocshArg *recinitArgs[] = { &recinitArg0 };
+
+static const iocshFuncDef gcbInitRecFuncDef = {
+    "gcbInitCmdRecord", 1, recinitArgs};
+
+static void gcbInitRecCallFunc(const iocshArgBuf *args)
+{
+    PVRecordPtr record;
+    std::string recordName = args[0].sval;
+    if(!recordName.c_str()) {
+        throw std::runtime_error("gcbStarRecThread: invalid number of arguments");
+    }
+    initGcbCmdRecord(recordName);
+}
+
+static const iocshFuncDef gcbStartThreadFuncDef = {
+    "gcbStartRecThread", 0, NULL};
+
+static void gcbStartThreadCallFunc(const iocshArgBuf *args)
+{
+    startRecThread();
+}
+
+static const iocshFuncDef gcbInitStatusClientDataSharingFuncDef = {
+    "gcbInitStatusClientDataShare", 0, NULL};
+
+static void gcbInitStatusClientDataSharingCallFunc(const iocshArgBuf *args)
+{
+    initGCBStatusDataShare();
+}
+
+static const iocshArg threadArg0 = { "threadName", iocshArgString };
+static const iocshArg *threadArgs[] = { &threadArg0 };
+
+static const iocshFuncDef gcbTestThreadFuncDef = {
+    "gcbLaunchThread", 1, threadArgs};
+
+static void gcbTestThreadCallFunc(const iocshArgBuf *args)
+{
+    char *threadName = args[0].sval;
+    if(!threadName) {
+        throw std::runtime_error("gcbLaunchThread: invalid number of arguments");
+    }
+    startTest(threadName);
+}
+
+static const iocshFuncDef gcbTestWakeThreadFuncDef = {
+    "gcbWakeThread", 0, NULL};
+
+static void gcbTestWakeThreadCallFunc(const iocshArgBuf *args)
+{
+    wakeThread();
+}
+
+static const iocshFuncDef stopThreadsFuncDef = {
+    "exitGracefully", 0, NULL};
+
+static void stopThreadsCallFunc(const iocshArgBuf *args)
+{
+    extReadThreadExit->signal();
+    rxP1Trigger->signal();
+    while(threadCounter){
+        epicsThreadSleep(0.005);
+    }
+    epicsExit(1);
+}
+
+static const iocshArg pvcltArg0 = { "pvcltName", iocshArgString };
+static const iocshArg *pvcltArgs[] = { &pvcltArg0 };
+
+static const iocshFuncDef gcbInitStsCltFuncDef = {
+    "gcbInitStatusPVAClient", 1, pvcltArgs};
+
+static void gcbInitStsCltCallFunc(const iocshArgBuf *args)
+{
+    char *pvcltName = args[0].sval;
+    if(!pvcltName) {
+        throw std::runtime_error("gcbInitStatusPVAClient: invalid number of arguments");
+    }
+    initGCBStatusClientRec(pvcltName);
+}
+
+static const iocshFuncDef gcbStartStsClientThreadFuncDef = {
+    "gcbStartStatusClientThread", 0, NULL};
+
+static void gcbStartStsClientThreadCallFunc(const iocshArgBuf *args)
+{
+    startGCBStatusClientThread();
+}
+
+static void gcbRegisterCppCommands(void)
+{
+    static int firstTime = 1;
+    if (firstTime) {
+        firstTime = 0;
+        iocshRegister(&gcbInitRecFuncDef, gcbInitRecCallFunc);
+        iocshRegister(&gcbInitStsCltFuncDef, gcbInitStsCltCallFunc);
+        iocshRegister(&gcbStartThreadFuncDef, gcbStartThreadCallFunc);
+        iocshRegister(&gcbTestThreadFuncDef, gcbTestThreadCallFunc);
+        iocshRegister(&gcbTestWakeThreadFuncDef, gcbTestWakeThreadCallFunc);
+        iocshRegister(&stopThreadsFuncDef, stopThreadsCallFunc);
+        iocshRegister(&gcbStartStsClientThreadFuncDef, gcbStartStsClientThreadCallFunc);
+        iocshRegister(&gcbInitStatusClientDataSharingFuncDef, gcbInitStatusClientDataSharingCallFunc);
+    }
+}
+
+extern "C" {
+    epicsExportRegistrar(gcbRegisterCppCommands);
+}
